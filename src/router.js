85bd305d958e8ad645d3f3b1e5943413be31928b
Andrea Gomez <andrea.vgomez27@gmail.com>
2023-08-06 23:48:36 -0300

    Proyecto final

--- src/router.js
+++ src/router.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import HelloWorld from "./components/HelloWorld.vue";
+
 Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: HelloWorld, name: "home" },
+];
 
 const router = new VueRouter({
   mode: "history",
